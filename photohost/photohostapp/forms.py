83d6b8ad4285a354f395871bea4285be692fbc81
ximg <nilsa8978@gmail.com>
2026-01-16 21:20:10 +0300

    fixed lifetime issue

--- photohost/photohostapp/forms.py
+++ photohost/photohostapp/forms.py
@@ -6,47 +6,27 @@ class ImageUploadForm(forms.Form):
 
 
 class SectionCreateForm(forms.ModelForm):
-
     LIFETIME_CHOICES = (
-        ("1d", _("1 day")),
-        ("3d", _("3 days")),
-        ("1w", _("1 week")),
-        ("2w", _("2 weeks")),
-        ("1m", _("1 month")),
-        ("2m", _("2 months")),
+        (1, _("1 day")),
+        (3, _("3 days")),
+        (7, _("1 week")),
+        (14, _("2 weeks")),
+        (30, _("1 month")),
+        (60, _("2 months")),
     )
 
-    lifetime = forms.ChoiceField(
+    lifetime_days = forms.TypedChoiceField(
         choices=LIFETIME_CHOICES,
-        initial="1w",
+        coerce=int,
+        initial=7,
         widget=forms.Select(attrs={"class": "form-select"}),
-        label=_("Deletion time")
+        label=_("Deletion time"),
     )
 
     class Meta:
         model = Section
-        fields = ["keep_original_filenames"]
-        labels = {
-            "keep_original_filenames": _("Save original file names"),
-        }
-        widgets = {
-            "keep_original_filenames": forms.CheckboxInput(
-                attrs={"class": "form-check-input"}
-            )
-        }
+        fields = ["keep_original_filenames", "lifetime_days"]
 
-    def clean_lifetime(self):
-        """Convert lifetime codes to days"""
-        mapping = {
-            "1d": 1,
-            "3d": 3,
-            "1w": 7,
-            "2w": 14,
-            "1m": 30,
-            "2m": 60,
-        }
-        value = self.cleaned_data["lifetime"]
-        return mapping[value]
 
 class MultiFileInput(forms.FileInput):
     allow_multiple_selected = True
